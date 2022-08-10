1c6b229023a91519bc235ff2bfdc41ad97149768
patoary <zahidsagar93@gmail.com>
2022-08-10 22:18:36 +0900

    Exceptional Dental Care

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -2,6 +2,7 @@ import React from 'react';
 import fluoride from '../../../assets/images/fluoride.png';
 import cavity from '../../../assets/images/cavity.png';
 import whitening from '../../../assets/images/whitening.png';
+import treatment from '../../../assets/images/treatment.png';
 import Service from './Service';
 const Services = () => {
     const services = [
@@ -30,13 +31,25 @@ const Services = () => {
                 <h3 className='text-secondary text-xl uppercase'>Our Services</h3>
                 <h2 className='text-4xl'>Services We Provide</h2>
             </div>
-            <div className='grid sm:grid-cols-1 m-auto md:grid-cols-2 lg:grid-cols-3 gap-9'>
+            <div className='grid sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-10'>
                 {
                     services.map(service => <Service
                         key={service._id}
                         service={service}
                     ></Service>)
                 }
+            </div>
+            <div>
+                <div className="hero min-h-screen xsm:mt-10">
+                    <div className="hero-content flex-col lg:flex-row">
+                        <img src={treatment} className="max-w-sm rounded-lg shadow-2xl" />
+                        <div className='lg:ml-28 xsm:mt-9'>
+                            <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+                            <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                            <button className="btn btn-primary text-white font-bold bg-gradient-to-r from-secondary to-primary">Get Started</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );

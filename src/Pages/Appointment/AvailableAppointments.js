c76056021fa38e1807277dd4e68bae5c6f431451
patoary <zahidsagar93@gmail.com>
2022-08-12 17:50:32 +0900

    fetch service from db

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -8,13 +8,13 @@ const AvailableAppointments = ({date}) => {
     const [treatment, setTreatment] = useState(null);
 
     useEffect( () =>{
-        fetch('services.json')
+        fetch('http://localhost:4000/service')
         .then(res => res.json())
         .then(data => setServices(data))
     },[]);
     return (
         <div>
-            <h3 className='text-xl text-secondary text-center m-10' >Available Appointments on {format(date, 'PP')}</h3>
+            <h3 className='text-xl text-secondary text-center my-10' >Available Appointments on {format(date, 'PP')}</h3>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5'>
                 {
                     services.map(service => <Service

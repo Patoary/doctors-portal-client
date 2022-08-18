44b18a04e3f30eabc03c67d4c8f9386324750818
patoary <zahidsagar93@gmail.com>
2022-08-18 19:55:47 +0900

    send a authorization headers in back end and handle res status in MyAppointments component

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,46 +1,65 @@
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const MyAppointments = () => {
-    const [appointments, setAppointments] = useState([]);
-    const [user] = useAuthState(auth);
-    useEffect(()=>{
-        if(user){
-            fetch(`http://localhost:4000/booking?patient=${user.email}`)
-        .then(res => res.json())
-        .then(data => setAppointments(data));
+  const [appointments, setAppointments] = useState([]);
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:4000/booking?patient=${user.email}`, {
+        method: 'GET',
+        headers: {
+          'authorization': `Bearer ${localStorage.getItem('accessToken')}`
         }
-    },[user]);
-    return (
-        <div>
-            <h3>My Appointments : {appointments.length}</h3>
-            <div class="overflow-x-auto">
-  <table class="table w-full">
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Date</th>
-        <th>Time</th>
-        <th>Treatment</th>
-      </tr>
-    </thead>
-    <tbody>
-        {
-            appointments.map((appointment, index) => <tr className='hover'>
+      })
+        .then(res => {
+          if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+            localStorage.removeItem('accessToken');
+            navigate('/');
+          }
+          return res.json()
+        })
+
+        .then(data => {
+
+          setAppointments(data)
+        });
+    }
+  }, [user]);
+  return (
+    <div>
+      <h3>My Appointments : {appointments?.length}</h3>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Treatment</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              appointments.map((appointment, index) => <tr key={index} className='hover'>
                 <th>{index + 1}</th>
                 <td>{appointment.patientName}</td>
                 <td>{appointment.date}</td>
                 <td>{appointment.slot}</td>
                 <td>{appointment.treatment}</td>
               </tr>)
-        }
-    </tbody>
-  </table>
-</div>
-        </div>
-    );
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyAppointments;

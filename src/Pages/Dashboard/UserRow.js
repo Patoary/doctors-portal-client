c98a53f8a9a2db3669d694d4d57772a907beabb5
patoary <zahidsagar93@gmail.com>
2022-08-19 17:08:40 +0900

    create a functional Make Admin button  and sand a authorization headers from UserRow

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,13 +1,28 @@
+import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
 import React from 'react';
-
-const UserRow = ({user,index}) => {
-    const {email} = user;
+import {toast} from 'react-toastify';
+const UserRow = ({user, index, refetch}) => {
+    const {email, role} = user;
+    const makeAdmin = () =>{
+        fetch(`http://localhost:4000/user/admin/${email}`,{
+            method: 'PUT',
+            headers:{
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            refetch()
+            toast('Successfully made an admin')
+        })
+    }
     return (
         <>
             <tr>
                 <th>{index + 1}</th>
                 <td>{email}</td>
-                <td>Quality Control Specialist</td>
+                <td>{role !== 'admin' && <button onClick={makeAdmin} className='btn btn-xs'>Make Admin</button>}</td>
+                <td><button className='btn btn-xs'>Remove User</button></td>
                 <td>Blue</td>
             </tr>
         </>
--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,37 +1,55 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
-import {useQuery} from 'react-query';
+import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import Loading from '../Shared/Loading';
 import UserRow from './UserRow';
 
 const Users = () => {
-    const {data: users, isLoading} = useQuery('users', () => fetch('http://localhost:4000/user').then(res => res.json()));
-    if(isLoading){
+    const navigate = useNavigate();
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:4000/user', {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        },
+    }).then(res => {
+        if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+            localStorage.removeItem('accessToken');
+            navigate('/');
+        }
+        return res.json()
+
+    }));
+    if (isLoading) {
         return <Loading></Loading>
     }
-        return (
+    return (
         <div>
-            <h2 className="text-2xl">All Users: {users.length}</h2>
-            <div class="overflow-x-auto">
-  <table class="table w-full">
-    <thead>
-      <tr>
-        <th></th>
-        <th>Email</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
-     {
-         users.map((user,index)=> <UserRow
-         key={user._id}
-         index= {index}
-         user= {user}
-         ></UserRow>)
-     }
-    </tbody>
-  </table>
-</div>
+            <h2 className="text-2xl">All Users: {users?.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Email</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users?.map((user, index) => <UserRow
+                                key={user._id}
+                                index={index}
+                                refetch={refetch}
+                                user={user}
+                            ></UserRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };

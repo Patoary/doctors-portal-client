24c4a1d65497583e042ade9f03eee3a592f5eac1
patoary <zahidsagar93@gmail.com>
2022-08-24 01:24:38 +0900

    create a DeleteConfirmModal

--- src/Pages/Dashboard/DoctorRow.js
+++ src/Pages/Dashboard/DoctorRow.js
@@ -1,26 +1,6 @@
 import React from 'react';
-import { toast } from 'react-toastify';
-
-const DoctorRow = ({ doctor, index, refetch }) => {
-    const { name, img, specialty, email} = doctor;
-
-    const handleDelete = email =>{
-        fetch(`http://localhost:4000/doctor/${email}`,{
-            method: 'DELETE',
-            headers:{
-                authorization: `Bearer ${localStorage.getItem('accessToken')}`
-            }
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            if(data.deletedCount){
-                toast.success(`Doctor: ${name} is deleted.`)
-                refetch();
-            }
-        
-        })
-    };
+const DoctorRow = ({ doctor, index, setDeletingDoctor }) => {
+    const { name, img, specialty} = doctor;
 
     return (
         <tr>
@@ -35,10 +15,8 @@ const DoctorRow = ({ doctor, index, refetch }) => {
             <td>{name}</td>
             <td>{specialty}</td>
             <td>
-                <button
-                 className='btn btn-xs bg-error'
-                 onClick={()=>handleDelete(email)}
-                 >Delete</button>
+            <label onClick={()=> setDeletingDoctor(doctor)} for="delete-confirm-modal" class="btn  btn-xs btn-error">Delete</label>
+               
                 </td>
         </tr>
     );
--- src/Pages/Dashboard/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import DeleteConfirmModal from './DeleteConfirmModal';
 import DoctorRow from './DoctorRow';
 
 const ManageDoctors = () => {
-    const {data: doctors, isLoading, refetch} = useQuery('doctors', ()=> fetch('http://localhost:4000/doctor', {
-        headers:{
+    const [deletingDoctor, setDeletingDoctor] = useState(null);
+    const { data: doctors, isLoading, refetch } = useQuery('doctors', () => fetch('http://localhost:4000/doctor', {
+        headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
         }
     }).then(res => res.json()));
 
-    if(isLoading){
+    if (isLoading) {
         return <Loading></Loading>
     }
     return (
@@ -18,28 +20,34 @@ const ManageDoctors = () => {
             <h2 className='text-2xl'>Manage Doctors: {doctors.length}</h2>
 
             <div class="overflow-x-auto">
-  <table class="table w-full">
-    <thead>
-      <tr>
-        <th></th>
-        <th>Avater</th>
-        <th>Name</th>
-        <th>Specialty</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-  {
-      doctors.map((doctor, index) => <DoctorRow
-      key={doctor._id}
-      doctor = {doctor}
-      index = {index}
-      refetch= {refetch}
-      ></DoctorRow>)
-  }
-    </tbody>
-  </table>
-</div>
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avater</th>
+                            <th>Name</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, index) => <DoctorRow
+                                key={doctor._id}
+                                doctor={doctor}
+                                index={index}
+                                refetch={refetch}
+                                setDeletingDoctor={setDeletingDoctor}
+                            ></DoctorRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+            {deletingDoctor && <DeleteConfirmModal
+            deletingDoctor={deletingDoctor}
+            refetch = {refetch}
+            setDeletingDoctor={setDeletingDoctor}
+            ></DeleteConfirmModal>}
         </div>
     );
 };
